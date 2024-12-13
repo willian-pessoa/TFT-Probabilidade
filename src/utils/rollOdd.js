fb8810f4189e796284ff3a24eb6e5407682cb5a0
willian-pessoa <willian.abreu@hittelco.com>
2024-12-13 11:55:23 -0300

    update: patch 14.24 odds and pool

--- src/utils/rollOdd.js
+++ src/utils/rollOdd.js
@@ -1,82 +1,113 @@
 // {championCost: [number of champions, number of champion copys]}
-const POOL = { 1: [13, 29], 2: [13, 22], 3: [13, 18], 4: [12, 12], 5: [8, 10] }
+const POOL = { 1: [14, 30], 2: [13, 25], 3: [13, 18], 4: [12, 10], 5: [8, 9] };
 
 // {levelShop: [odd_T1, odd_T2, odd_T3, odd_T4, odd_T5]}
 const SHOP_ODDS = {
   1: [1, 0, 0, 0, 0],
   2: [1, 0, 0, 0, 0],
   3: [0.75, 0.25, 0, 0, 0],
-  4: [0.55, 0.30, 0.15, 0, 0],
-  5: [0.45, 0.33, 0.20, 0.02, 0],
-  6: [0.25, 0.40, 0.30, 0.05, 0],
-  7: [0.19, 0.30, 0.35, 0.15, 0.01],
-  8: [0.16, 0.20, 0.35, 0.25, 0.04],
-  9: [0.09, 0.15, 0.30, 0.30, 0.16]
-}
+  4: [0.55, 0.3, 0.15, 0, 0],
+  5: [0.45, 0.33, 0.2, 0.02, 0],
+  6: [0.3, 0.4, 0.25, 0.05, 0],
+  7: [0.19, 0.3, 0.4, 0.1, 0.01],
+  8: [0.18, 0.25, 0.32, 0.22, 0.03],
+  9: [0.15, 0.2, 0.25, 0.3, 0.1],
+};
 
 export const formatPercentage = (number) => {
-  return (number * 100).toFixed(2) + " %"
-}
+  return (number * 100).toFixed(2) + " %";
+};
 
-export const computeOneChamp = (cost, level, rolls = 1, sameOutPool = 0, costOutPool = 0) => {
-  let champChance = (POOL[cost][1] - sameOutPool) / ((POOL[cost][0] * POOL[cost][1]) - costOutPool)
-  let shopChance = SHOP_ODDS[level][cost - 1]
-  let totalChance = champChance * shopChance * 5
-  if (rolls === 1) return totalChance
+export const computeOneChamp = (
+  cost,
+  level,
+  rolls = 1,
+  sameOutPool = 0,
+  costOutPool = 0
+) => {
+  let champChance =
+    (POOL[cost][1] - sameOutPool) /
+    (POOL[cost][0] * POOL[cost][1] - costOutPool);
+  let shopChance = SHOP_ODDS[level][cost - 1];
+  let totalChance = champChance * shopChance * 5;
+  if (rolls === 1) return totalChance;
 
   for (let i = 1; i < rolls; i++) {
-    let chanceNotFind = 1 - totalChance
-    totalChance = totalChance + (chanceNotFind * champChance * shopChance * 5)
+    let chanceNotFind = 1 - totalChance;
+    totalChance = totalChance + chanceNotFind * champChance * shopChance * 5;
   }
 
-  return totalChance
-}
+  return totalChance;
+};
 
-export const tableOfOdds = (cost, level, size = 25, sameOutPool = 0, costOutPool = 2 * 8) => {
-  let TABLE_OF_ODDS = {}
+export const tableOfOdds = (
+  cost,
+  level,
+  size = 25,
+  sameOutPool = 0,
+  costOutPool = 2 * 8
+) => {
+  let TABLE_OF_ODDS = {};
 
   for (let i = 1; i <= size; i++) {
-    const key = "roll_" + i
-    TABLE_OF_ODDS[key] = formatPercentage(computeOneChamp(cost, level, i, sameOutPool, costOutPool))
+    const key = "roll_" + i;
+    TABLE_OF_ODDS[key] = formatPercentage(
+      computeOneChamp(cost, level, i, sameOutPool, costOutPool)
+    );
   }
 
-  return TABLE_OF_ODDS
-}
+  return TABLE_OF_ODDS;
+};
 
-export const simuladorJogo = (nJogos, rolls, cost, level, foraDaPool = 0, custoForaPool = 0) => {
+export const simuladorJogo = (
+  nJogos,
+  rolls,
+  cost,
+  level,
+  foraDaPool = 0,
+  custoForaPool = 0
+) => {
   // {jogo: numero de copias}
-  let JOGOS = {}
+  let JOGOS = {};
 
   for (let i = 1; i <= nJogos; i++) {
-    let key = "Jogo_" + i
-    let copias = 0
-    let total_rolls = rolls
-    let current_roll = 1
-    let rolls_whithout_find = 1
+    let key = "Jogo_" + i;
+    let copias = 0;
+    let total_rolls = rolls;
+    let current_roll = 1;
+    let rolls_whithout_find = 1;
 
     while (current_roll <= total_rolls) {
-      let chance = computeOneChamp(cost, level, rolls_whithout_find, copias + foraDaPool, custoForaPool)
-      let isFound = chance > Math.random()
+      let chance = computeOneChamp(
+        cost,
+        level,
+        rolls_whithout_find,
+        copias + foraDaPool,
+        custoForaPool
+      );
+      let isFound = chance > Math.random();
 
       if (isFound) {
-        rolls_whithout_find = 1
-        copias++
+        rolls_whithout_find = 1;
+        copias++;
       } else {
-        rolls_whithout_find++
+        rolls_whithout_find++;
       }
 
-      current_roll++
+      current_roll++;
     }
 
-    JOGOS[key] = copias
+    JOGOS[key] = copias;
   }
 
-  const RESULT = {}
+  const RESULT = {};
 
-  Object.values(JOGOS).forEach(copias => RESULT[copias] ? RESULT[copias]++ : RESULT[copias] = 1)
+  Object.values(JOGOS).forEach((copias) =>
+    RESULT[copias] ? RESULT[copias]++ : (RESULT[copias] = 1)
+  );
 
-  return RESULT
-}
+  return RESULT;
+};
 
 // const TOTAL_JOGOS = 500
 // const RESULT = simuladorJogo(TOTAL_JOGOS, 40, 3, 7, 6, 6 * 8)
